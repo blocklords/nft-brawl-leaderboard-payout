abdb4c29f909211ab55af1518d791a40a25846f9
ahmetson <admin@blocklords.io>
2021-07-15 20:45:16 +0700

    move blockchain utility to utils folder

--- utils/blockchain.js
+++ utils/blockchain.js
@@ -1,11 +1,11 @@
 let Web3 = require('web3');
 let fs = require('fs');
 
-if (process.env.REMOTE_HTTP == undefined) {
-    throw "No REMOTE_HTTP environment variable found. Can not connect to blockchain";
+if (process.env.BLOCKCHAIN_ENDPOINT == undefined) {
+    throw "No BLOCKCHAIN_ENDPOINT environment variable found. Can not connect to blockchain";
     process.exit(1);
 }
-let web3 = new Web3(process.env.REMOTE_HTTP);
+let web3 = new Web3(process.env.BLOCKCHAIN_ENDPOINT);
 
 let addAccount = function(privateKey) {
     // add account from privatekey to web3 to sign contracts
@@ -32,19 +32,19 @@ let call = async function(instance, method, address, args) {
 
 let loadCrowns = async function() {
 	// address and abi of the Crowns (CWS) erc-20 token
-	var crownsArtifact = JSON.parse(fs.readFileSync('./abi/CrownsToken.json', 'utf8'));
+	var crownsArtifact = JSON.parse(fs.readFileSync('./abi/crowns.json', 'utf8'));
 	var crownsAddress = process.env.CROWNS_ADDRESS;
-	var crowns = await loadContract(crownsAddress, crownsArtifact.abi);
+	var crowns = await loadContract(crownsAddress, crownsArtifact);
 
 	return crowns;
 };
 
-let loadNftRush = async function() {
+let loadNftBrawl = async function() {
 	// address and abi of second game: nft rush
-	var artifact = JSON.parse(fs.readFileSync('./abi/NftRush.json', 'utf8'));
+	var artifact = JSON.parse(fs.readFileSync('./abi/nft_brawl.json', 'utf8'));
 
-	var nftRushAddress = process.env.NFT_RUSH_ADDRESS;	
-	var nftRush = await loadContract(nftRushAddress, artifact.abi);	
+	var nftRushAddress = process.env.NFT_BRAWL_ADDRESS;	
+	var nftRush = await loadContract(nftRushAddress, artifact);	
 
 	return nftRush;
 };
@@ -53,5 +53,5 @@ module.exports.addAccount = addAccount;
 module.exports.loadContract = loadContract;
 module.exports.call = call;
 module.exports.loadCrowns = loadCrowns;
-module.exports.loadNftRush = loadNftRush;
+module.exports.loadNftBrawl = loadNftBrawl;
 module.exports.web3 = web3;
